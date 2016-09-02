1c24ea071783bc5c00f58b4f3ec8d24bb78532e6
yangxianhao <513532865@qq.com>
2016-09-02 22:02:52 +0800

    Initial Commit

---
+++ OrderCourse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrderCourse
+//
+//  Created by 杨先豪 on 16/9/2.
+//  Copyright © 2016年 yangxianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrderCourse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrderCourse
+//
+//  Created by 杨先豪 on 16/9/2.
+//  Copyright © 2016年 yangxianhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
