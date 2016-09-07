b402578ece9e88e8bac80544bab307fe1e1d13ba
yangxianhao <513532865@qq.com>
2016-09-07 23:15:11 +0800

    删除openid,userid

--- OrderCourse/Main/XHConstant.m
+++ OrderCourse/Main/XHConstant.m
@@ -12,9 +12,9 @@
 
 @end
 // 可变常量
-NSString *const openid = @"oXoOjt9tI9bAOy0TVyt4CvZLDwDQ";
-NSString *const userId = @"660aaf5e-b7ab-4157-b333-ae2e3328ab02";
-NSString *const contractGuid = @"ad36003e-5b52-e611-80d1-02bf0a00005b";
+NSString *const openid = @"";
+NSString *const userId = @"";
+NSString *const contractGuid = @"";
 
 // 不变常量
 NSString *const baseURL = @"http://ols.webi.com.cn";
